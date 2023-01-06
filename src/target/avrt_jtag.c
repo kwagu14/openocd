e29719035e557de6b59379de7d115910003a2277
Karley <karley.waguespack.gmail.com>
2023-01-06 16:44:49 -0600

    added headers to the avrt_mem.c and avrt_jtag.c

--- src/target/avrt_jtag.c
+++ src/target/avrt_jtag.c
@@ -1,3 +1,18 @@
+/************************************************
+* Author: Karley W.								* 
+* 												*
+* File Purpose: uses low-level JTAG programming	*
+* commands to accomplish typical debugging 		*
+* tasks											*
+* 												*
+* ATMEGA32 chips have an on-chip debug system, 	*
+* but the JTAG instructions to use it are 		*
+* proprietary. Instead, the programming 		*
+* interface will be used to implement read and	*
+* write functions.								*
+*												*
+************************************************/
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -6,39 +21,3 @@
 #include "jtag/jtag.h"
 #include "avrt_jtag.h"
 
-
-static int avr_jtag_mwa_read_data(struct mcu_jtag *jtag_info,uint8_t *pdata)
-{
-
-	struct scan_field fields[2];
-	uint8_t data_buf[1];
-	uint8_t busy_buf[4];
-	int busy;
-
-	do {
-		memset(data_buf, 0, sizeof(data_buf));
-		memset(busy_buf, 0, sizeof(busy_buf));
-
-		fields[0].num_bits = 8;
-		fields[0].out_value = NULL;
-		fields[0].in_value = data_buf;
-
-
-		fields[1].num_bits = 3;
-		fields[1].in_value = busy_buf;
-		fields[1].out_value = NULL;
-
-		jtag_add_dr_scan(jtag_info->tap, 2, fields, TAP_IDLE);
-
-		if (jtag_execute_queue() != ERROR_OK) {
-			LOG_ERROR("%s: reading data  failed", __func__);
-			return ERROR_FAIL;
-		}
-
-		busy = busy_buf[0];
-	} while (busy);
-
-	buf_cpy(data_buf, pdata, 8);
-
-	return ERROR_OK;
-}
--- src/target/avrt_mem.c
+++ src/target/avrt_mem.c
@@ -1,3 +1,17 @@
+/************************************************
+* Author: Karley W.								* 
+* 												*
+* File Purpose: Handles higher-level read and 	*
+* write capabilities for 8-bit AVR chips. 		*
+*												*
+* Note: this file was modeled after the 32-bit 	*
+* AVR driver, but reading and writing on 8-bit 	*
+* chips follows a very different process. Will 	*
+* probably need modification after jtag file is *
+* created. 										*
+* 												*
+************************************************/
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
